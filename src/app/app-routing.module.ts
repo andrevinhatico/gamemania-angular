3632f81855396b8ae0a89c9f3ec8adbf1de2ed6f
Andre Ribeiro <andrebahia7@gmail.com>
2022-01-20 15:02:22 -0300

    passando html , css e bootstrap para o angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConsolesComponent } from './views/consoles/consoles.component';
+import { HomeComponent } from './views/home/home.component';
+import { PlaystationComponent } from './views/playstation/playstation.component';
+import { XboxComponent } from './views/xbox/xbox.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: "", component: HomeComponent},
+
+  {path: "consoles", component: ConsolesComponent},
+
+  {path: "playstation", component: PlaystationComponent},
+
+  {path: "xbox", component: XboxComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './views/home/home.component';
+import { HeaderComponent } from './views/header/header.component';
+import { FooterComponent } from './views/footer/footer.component';
+import { ConsolesComponent } from './views/consoles/consoles.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PlaystationComponent } from './views/playstation/playstation.component';
+import { XboxComponent } from './views/xbox/xbox.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    ConsolesComponent,
+    PlaystationComponent,
+    XboxComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
